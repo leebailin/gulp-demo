d3c803c6a989d6a3ae74a945548745a055a4957f
bailin <931533034@qq.com>
2020-09-03 13:40:06 +0800

    feat: add package && perfect

---
+++ pages.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  data: {
+    menus: [
+      {
+        name: 'Home',
+        icon: 'aperture',
+        link: 'index.html'
+      },
+      {
+        name: 'Features',
+        link: 'features.html'
+      },
+      {
+        name: 'About',
+        link: 'about.html'
+      },
+      {
+        name: 'Contact',
+        link: '#',
+        children: [
+          {
+            name: 'Github',
+            link: 'https://github.com/leebailin'
+          },
+          {
+            name: 'About',
+            link: 'https://baidu.com/'
+          },
+          {
+            name: 'divider'
+          },
+          {
+            name: 'About',
+            link: 'https://github.com/leebailin'
+          }
+        ]
+      }
+    ],
+    pkg: require('./package.json'),
+    date: new Date()
+  },
+  build: {
+    temp: '.temp',
+    dist: 'release'
+  }
+}
